2f4c2ed4c1a8ae597661673e3e2734a892f5be9a
Ahmed Jadani <ahmed4star@gmail.com>
2025-07-28 10:52:16 +0100

    Fix diff display issue with complete file content

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
-@@ .. @@
- import { createApp } from 'vue'
--import { createPinia } from 'pinia'
- 
- import App from './App.vue'
--import router from './router'
-+import './style.css'
- 
- const app = createApp(App)
- 
--app.use(createPinia())
--app.use(router)
--
- app.mount('#app')
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import './style.css'
+
+const app = createApp(App)
+
+app.mount('#app')
