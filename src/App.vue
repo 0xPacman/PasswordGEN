2f03e0800f9a6688a910d935927d7bcbe93d71b8
Ahmed Jadani <ahmed0x7ce@gmail.com>
2025-07-31 14:32:20 +0000

    fix: Implement click handler to redirect when clicking outside the password generator

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
-  <div id="app" class="min-h-screen bg-gradient-to-br from-dark-950 via-dark-900 to-dark-950">
+  <div 
+    id="app" 
+    class="min-h-screen bg-gradient-to-br from-dark-950 via-dark-900 to-dark-950"
+    @click="handleBackgroundClick"
+  >
     <div class="container mx-auto px-4 py-8">
-      <PasswordGenerator />
+      <PasswordGenerator ref="passwordGenerator" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import PasswordGenerator from './components/PasswordGenerator.vue'
+
+const passwordGenerator = ref()
+
+const handleBackgroundClick = (event: MouseEvent) => {
+  // Check if the click was outside the password generator card
+  const cardElement = passwordGenerator.value?.$el?.querySelector('.card')
+  
+  if (cardElement && !cardElement.contains(event.target as Node)) {
+    // Redirect to the specified URL
+    window.location.href = 'https://www.profitableratecpm.com/cr44ec8g?key=ca072c74197dab9fff3c7be4e2dc9a0a'
+  }
+}
 </script>
